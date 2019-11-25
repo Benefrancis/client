b2c62e2d556b3403bf638cbf13e842da9874fbbc
Benefrancis <benefrancis@gmail.com>
2019-11-25 01:28:23 -0300

    Alistagem de clients só será exibida para usuários autenticados com o perfil de administrador

--- src/main/java/br/com/benefrancis/client/ClientApplication.java
+++ src/main/java/br/com/benefrancis/client/ClientApplication.java
@@ -2,10 +2,12 @@ package br.com.benefrancis.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ClientApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/benefrancis/client/controller/ClientRestController.java
+++ src/main/java/br/com/benefrancis/client/controller/ClientRestController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class ClientRestController {
 	private ClientService service;
 
 	@GetMapping("/")
+	@PreAuthorize("hasRole('ROLE_admin')")
 	public List<ClientDto> findAll() {
 		List<ClientDto> resposta = new ArrayList<>();
 		List<Client> all = service.findAll();
